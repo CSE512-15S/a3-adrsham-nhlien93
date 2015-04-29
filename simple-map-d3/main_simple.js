81da2c6d0dbb34d979f7a5b973c5a414f5446364
Lindsey Nguyen <nhlien93@gmail.com>
2015-04-29 11:32:23 -0700

    commiting simple d3 map stuff

---
+++ simple-map-d3/main_simple.js
@@ -0,0 +1,59 @@
+(function($) {
+        $(document).ready(function() {
+		  var waMap = SimpleMapD3({
+            container: '.simple-map-d3-wa-map',
+            datasource: 'example-data/wa_county.geojson',
+            projection: 'mercator'
+          });
+	/*	  
+          var mnPopMap = SimpleMapD3({
+            container: '.simple-map-d3-mn-pop-map',
+            datasource: 'example-data/mn-county-2010.geo.json',
+            colorOn: true,
+            colorProperty: 'POPULATION',
+            legendFormatter: d3.format(',f0')
+          });
+
+          var usMap = SimpleMapD3({
+            container: '.simple-map-d3-us-map',
+            datasource: 'example-data/us-counties.topo.json',
+            tooltipOn: false,
+            styles: {
+              stroke: '#EDEDED',
+              fill: '#232323'
+            }
+          });
+
+          var europePopMap = SimpleMapD3({
+            container: '.simple-map-d3-europe-pop-map',
+            datasource: 'example-data/europe-population-density-geocommons.geo.json',
+            colorSet: 'Spectral',
+            colorOn: true,
+            colorProperty: 'population',
+            colorReverse: true,
+            projection: 'azimuthalEqualArea',
+            rotation: [0, 0, -20],
+            canvasDragOn: true,
+            tooltipContent: function(d) {
+              var p = d.properties;
+              return '<h5>' + p.country + '</h5>' +
+                p.population + ' population per square kilometer';
+            }
+          });
+
+          var worldMap = SimpleMapD3({
+            container: '.simple-map-d3-world-map',
+            datasource: 'example-data/world-population.geo.json',
+            projection: 'equirectangular',
+            colorOn: true,
+            colorProperty: 'POP2005',
+            colorSet: 'Paired',
+            colorScale: 'quantize',
+            tooltipOn: true,
+            graticuleOn: true,
+            globeOn: true,
+            legendOn: false,
+            startManually: true
+          }).start();*/
+        });
+      })(jQuery);
